ebe9fd57ee95c3424983991af24912f617740434
Adithyan S Anil <ad7.adithyan@gmail.com>
2026-05-29 10:34:46 +0530

    datas preprocessed( reduced to same scale)

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
 df = pd.read_csv("data/diabetes.csv")
 x=df.iloc[:,0:8]
 y=df.iloc[:,-1]
+
+mm=MinMaxScaler()
+mm.fit(x)
+mx=mm.transform(x)
