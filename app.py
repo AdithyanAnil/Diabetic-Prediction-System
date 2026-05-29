505bf2e8cca4452de806bdc39f604e4f1cd917b8
Adithyan S Anil <ad7.adithyan@gmail.com>
2026-05-29 10:37:02 +0530

    train split created

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
 
 df = pd.read_csv("data/diabetes.csv")
 x=df.iloc[:,0:8]
@@ -8,3 +9,5 @@ y=df.iloc[:,-1]
 mm=MinMaxScaler()
 mm.fit(x)
 mx=mm.transform(x)
+
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
