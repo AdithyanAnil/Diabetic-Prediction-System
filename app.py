8ca69c0605eff5af65559a79aa86f79941a11a06
Adithyan S Anil <ad7.adithyan@gmail.com>
2026-05-29 10:39:56 +0530

    knn apllied

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split
-
+from sklearn.neighbors import KNeighborsClassifier
 df = pd.read_csv("data/diabetes.csv")
 x=df.iloc[:,0:8]
 y=df.iloc[:,-1]
@@ -11,3 +11,7 @@ mm.fit(x)
 mx=mm.transform(x)
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=42)
+
+knn=KNeighborsClassifier()
+knn.fit(x_train,y_train)
+y_predict=knn.predict(x_test)
